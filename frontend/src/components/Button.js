fc4fdc32d072c22755325865eec3bbfd81b2ddef
Denyse <mdenyse15@gmail.com>
2022-03-24 16:27:55 +0200

    refactor

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -2,7 +2,7 @@ export default function Button({ title = "Click me", ...props }) {
   return (
     <div className="mt-12">
       <button
-        className="bg-blue-800 text-white block w-full py-5 rounded text-xl focus:ring-4 focus:ring-blue-300 transition"
+        className="bg-blue-800 text-white block w-full py-5 rounded text-xl focus:ring-4 focus:ring-blue-400 transition"
         {...props}
       >
         {title}
