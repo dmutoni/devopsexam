e42b7eb457c84766a2dc828fd2bcb3f4719f035f
Denyse <mdenyse15@gmail.com>
2022-03-24 18:31:52 +0200

    fixed nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,66 @@
 import "./App.css";
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from "react-router-dom";
 import BuyElectricity from "./pages/BuyElectricty";
 import LoadElectricty from "./pages/LoadElectricty";
 import Check from "./pages/Check";
 
 const Home = () => {
-    return <div>
-        jjj
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-purple-800">
+        Hello welcome to Electricty billing
+      </h1>
+      <h1 className="text-6xl pt-4 font-bold text-purple-800">
+        You can browse freely
+      </h1>
     </div>
-}
+  );
+};
 
 const NotFound = () => (
-    <div>
-        <h1 className="text-6xl font-bold text-purple-800">404 - Not found</h1>
-    </div>
-)
-
+  <div>
+    <h1 className="text-6xl font-bold text-purple-800">404 - Not found</h1>
+  </div>
+);
 
 function App() {
-    
-    return (
-        <div>
-            <div className="flex px-12 py-5 shadow-lg">
-                <div className="px-4 cursor-pointer hover:underline"><Link to="/"> Home </Link></div>
-                <div className="px-4 cursor-pointer hover:underline"><Link to="/electricity/buy"> Buy Electricty </Link></div>
-                <div className="px-4 cursor-pointer hover:underline"><Link to="/electricity/load"> Load Electricty </Link></div>
-                <div className="px-4 cursor-pointer hover:underline"><Link to="/electricity/check"> Check electricty </Link></div>
-            </div>
-            <div className="h-[90vh] flex justify-center items-center">
-                <Routes>
-                    <Route path="/" exact element={<Home/>}> </Route>
-                    <Route path="/electricity/buy" element={<BuyElectricity/>}> </Route>
-                    <Route path="/electricity/load" element={<LoadElectricty/>}> </Route>
-                    <Route path="/electricity/check" element={<Check/>}> </Route>
-                    <Route path="*" element={<NotFound/>}>ddd</Route>
-                </Routes>
-            </div>
+  return (
+    <div>
+      <div className="flex px-12 py-5 shadow-lg">
+        <div className="px-4 cursor-pointer hover:underline">
+          <Link to="/"> Home </Link>
         </div>
-    );
+        <div className="px-4 cursor-pointer hover:underline">
+          <Link to="/electricity/buy"> Buy Electricty </Link>
+        </div>
+        <div className="px-4 cursor-pointer hover:underline">
+          <Link to="/electricity/load"> Load Electricty </Link>
+        </div>
+        <div className="px-4 cursor-pointer hover:underline">
+          <Link to="/electricity/check"> Check electricty </Link>
+        </div>
+      </div>
+      <div className="h-[90vh] flex justify-center items-center">
+        <Routes>
+          <Route path="/" exact element={<Home />}>
+            {" "}
+          </Route>
+          <Route path="/electricity/buy" element={<BuyElectricity />}>
+            {" "}
+          </Route>
+          <Route path="/electricity/load" element={<LoadElectricty />}>
+            {" "}
+          </Route>
+          <Route path="/electricity/check" element={<Check />}>
+            {" "}
+          </Route>
+          <Route path="*" element={<NotFound />}>
+            ddd
+          </Route>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
