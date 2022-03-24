6709978d5996fe7755826244c4f30003d639a744
Denyse <mdenyse15@gmail.com>
2022-03-24 16:23:02 +0200

    feat: Edited some parts

--- frontend/src/pages/BuyElectricty.js
+++ frontend/src/pages/BuyElectricty.js
@@ -4,38 +4,65 @@ import Button from "../components/Button";
 import axios from "axios";
 
 export default function BuyElectricity() {
-    const [ meter_number, setMeterNumber ] = useState(0)
-    const [ amount, setAmount ] = useState(0)
-    const [ error, setErorr ] = useState("")
-    const [ isSuccess, setIsSucess ] = useState(false)
-    const [ token, setToken ] = useState()
-    
-    const buyElectricty = async () => {
-        try {
-            let { data } = await axios.post("http://localhost:8001/api/tokens/buy", { meter_number, amount })
-            
-            setErorr("")
-            setIsSucess(true)
-            setToken(data.body.token)
-            console.log(data)
-        } catch (e) {
-            setIsSucess(false)
-            setErorr(e.response.data.message)
-        }
+  const [meter_number, setMeterNumber] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [error, setErorr] = useState("");
+  const [isSuccess, setIsSucess] = useState(false);
+  const [token, setToken] = useState();
+
+  const buyElectricty = async () => {
+    try {
+      let { data } = await axios.post("http://localhost:8001/api/tokens/buy", {
+        meter_number,
+        amount,
+      });
+
+      setErorr("");
+      setIsSucess(true);
+      setToken(data.body.token);
+      console.log(data);
+    } catch (e) {
+      setIsSucess(false);
+      setErorr(e.response.data.message);
     }
-    
-    return (
-        <div>
-            <h1 className="text-3xl font-medium" id="_buy_electricity_page">Buy Electricty</h1>
-            {error !== "" && <div className="py-10 text-red-500 w-[400px]" id="ErrorMessage">{error}</div>}
-            {isSuccess &&
-                <div className="py-10 text-lg text-blue-500 w-[400px]" id="SuccessMessage">Sucessfully Token bought :{token} ..</div>}
-            <div>
-                <Input type="number" placeholder="Meter_number" data={{ st: meter_number, sts: setMeterNumber }}/>
-                <Input type="number" placeholder="Amount" data={{ st: amount, sts: setAmount }}/>
-                
-                <Button action="buyElectricity" title="Buy" onClick={buyElectricty}/>
-            </div>
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-medium" id="_buy_electricity_page">
+        You can buy your electricity here
+      </h1>
+      {error !== "" && (
+        <div className="py-10 text-red-500 w-[400px]" id="ErrorMessage">
+          {error}
         </div>
-    )
+      )}
+      {isSuccess && (
+        <div
+          className="py-10 text-lg text-blue-500 w-[400px]"
+          id="SuccessMessage"
+        >
+          Successfully Token bought :{token} ..
+        </div>
+      )}
+      <div>
+        <Input
+          type="number"
+          placeholder="Meter_number"
+          data={{ st: meter_number, sts: setMeterNumber }}
+        />
+        <Input
+          type="number"
+          placeholder="Amount"
+          data={{ st: amount, sts: setAmount }}
+        />
+
+        <Button
+          action="buyElectricity"
+          title="Buy Electricity"
+          onClick={buyElectricty}
+        />
+      </div>
+    </div>
+  );
 }
